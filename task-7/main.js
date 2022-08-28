ba1ba0f4e2a4ac39151116f793c50b437f279cc1
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-28 21:39:26 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+       let n=result.input.split(" ");
+  
+    let arr1=[14,17,16,3,7,19,9];
+let arr2=[];
+    for (let i = 1; i < arr1.length; i++) {
+        if (arr1[i]%2!==0) {
+            arr2.push(arr1[i]);
+            
+        }
+        
+    }console.log(arr2);
   
 });
