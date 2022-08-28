ec2de2232f212deb8807d47a6c22133ac8a0db47
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-28 16:25:47 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,11 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+     let arr=[1,2,3,4]
+
+    for (let i = arr.length-1; i >= 0; i--) {
+        console.log(arr[i]);
+        
+    }
   
 });
