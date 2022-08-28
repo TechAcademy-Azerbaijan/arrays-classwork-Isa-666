8973ba9eac701c21cd7b806bdf32e9b1b7061935
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-28 23:50:25 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+       const arr1 = [1, 2, 3];
+    let b = arr1.map((i) => i + 2);
+    let arr2 = [-4]
+    let arr3 = b.concat(arr2);
+    console.log(arr3);
   
 });
