4090b79fa3514d7174c7085d33f18a9914aee5d9
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-29 01:47:41 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,23 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+           let a = result.input.split(" ")
+    let max = 0;
+
+    let arr = [];
+    for (let i = 0; i <= a.length; i++) {
+        arr.push(a[i]);
+        if (parseInt(a[i]) > max) {
+            max = parseInt(a[i]);
+        }
+
+    }
+    let count = 0;
+    for (let i = 1; i < arr.length; i++) {
+
+        if (max == parseInt(a[i])) {
+            count++;
+        }
+    } console.log(count);
   
 });
